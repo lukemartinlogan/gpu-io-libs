c80bf3641158417d5326bd6680d2406ed61d29f3
abscosmos <joshua@dinoslice.com>
2025-07-10 10:45:07 -0500

    Move non-HDF5 specific code into separate struct & function
    Will make it easier to use both C and C++ api

--- example/basic_hdf5/src/main.cpp
+++ example/basic_hdf5/src/main.cpp
@@ -10,52 +10,64 @@
 #include "hwinfo.h"
 #include "sample.h"
 
-int main() {
-    std::filesystem::path file_path = "./system_info.h5";
+struct SamplerParams {
+    std::filesystem::path file_path;
 
     std::string group_name;
     uint64_t sample_ms;
     uint16_t sample_ct;
+};
+
+std::vector<Sample> collect_samples(uint64_t sample_ms, uint16_t sample_ct) {
+    std::vector<Sample> samples;
+    samples.reserve(sample_ct);
+
+    for (size_t s = 0; s < sample_ct; ++s) {
+        samples.push_back({
+            .timestamp_ms = current_time_ms(),
+            .memory_kb = mem_kb(),
+            .cpu_time = cpu_time(),
+        });
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(sample_ms));
+    }
+
+    return samples;
+}
+
+int main() {
+    SamplerParams params{};
+
+    params.file_path = "./system_info.h5";
 
     std::cout << "Group name: ";
-    std::getline(std::cin, group_name);
+    std::getline(std::cin, params.group_name);
 
     std::cout << "Sample count: ";
-    if (!(std::cin >> sample_ct)){
+    if (!(std::cin >> params.sample_ct)){
         throw std::runtime_error("given sample count was not integer");
     }
 
     std::cout << "Sample interval (ms): ";
-    if (!(std::cin >> sample_ms)){
+    if (!(std::cin >> params.sample_ms)){
         throw std::runtime_error("given sample interval was not integer");
     }
 
     try {
-        unsigned int flags = std::filesystem::exists(file_path) ? H5F_ACC_RDWR : H5F_ACC_TRUNC;
+        unsigned int flags = std::filesystem::exists(params.file_path) ? H5F_ACC_RDWR : H5F_ACC_TRUNC;
 
-        H5::H5File file(file_path, flags);
+        H5::H5File file(params.file_path, flags);
 
-        H5::Group run_group = file.createGroup(group_name);
+        H5::Group run_group = file.createGroup(params.group_name);
 
-        hsize_t dims[1] = { sample_ct };
+        hsize_t dims[1] = { params.sample_ct };
         H5::DataSpace dataspace(1, dims);
 
         H5::CompType data_type = Sample::h5_comp_ty();
 
         H5::DataSet dataset = run_group.createDataSet("samples", data_type, dataspace);
 
-        std::vector<Sample> samples;
-        samples.reserve(sample_ct);
-
-        for (size_t s = 0; s < sample_ct; ++s) {
-            samples.push_back({
-                .timestamp_ms = current_time_ms(),
-                .memory_kb = mem_kb(),
-                .cpu_time = cpu_time(),
-            });
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(sample_ms));
-        }
+        std::vector samples = collect_samples(params.sample_ms, params.sample_ct);
 
         dataset.write(samples.data(), data_type);
 
@@ -66,12 +78,12 @@ int main() {
         unit_attr.write(str_ty, "timestamp: ms, cpu: %, memory: kB");
 
         run_group.createAttribute("sampling_interval_ms", H5::PredType::NATIVE_UINT64, scalar_space)
-            .write(H5::PredType::NATIVE_UINT64, &sample_ms);
+            .write(H5::PredType::NATIVE_UINT64, &params.sample_ms);
 
         run_group.createAttribute("sample_count", H5::PredType::NATIVE_UINT16, scalar_space)
-            .write(H5::PredType::NATIVE_UINT16, &sample_ct);
+            .write(H5::PredType::NATIVE_UINT16, &params.sample_ct);
 
-        std::cout << std::format("wrote data to {} in {}.", dataset.getObjName(), file_path.string()) << std::endl;
+        std::cout << std::format("wrote data to {} in {}.", dataset.getObjName(), params.file_path.string()) << std::endl;
 
         return 0;
     } catch (H5::Exception& e) {
