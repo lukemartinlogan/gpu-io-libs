2d972798ad6ae479a39b5d8da5031418223d25de
abscosmos <joshua@dinoslice.com>
2025-07-18 10:23:34 -0500

    Add missing field & add comments for ObjectHeader

--- implementation/src/hdf5/object_header.h
+++ implementation/src/hdf5/object_header.h
@@ -18,9 +18,16 @@ struct ObjectHeaderMessage {
 };
 
 struct ObjectHeader {
-    uint16_t header_message_count;
-    uint64_t object_ref_count;
-    std::vector<ObjectHeaderMessage> messages;
+    // total number of messages listed
+    // includes continuation messages
+    uint16_t message_count{};
+    // number of hard links to this object in the current file
+    uint64_t object_ref_count{};
+    // number of bytes of header message data for this header
+    // does not include size of object header continuation blocks
+    uint64_t object_header_size{};
+    // messages
+    std::vector<ObjectHeaderMessage> messages{};
 
     void Serialize(Serializer& s) const;
 
