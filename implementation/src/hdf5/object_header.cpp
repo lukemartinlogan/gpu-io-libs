8b4643203b524a01ba5de3b8b36ce2efd6633aee
abscosmos <joshua@dinoslice.com>
2025-07-18 10:24:59 -0500

    Add serialization method for ObjectHeader

--- implementation/src/hdf5/object_header.cpp
+++ implementation/src/hdf5/object_header.cpp
@@ -1,5 +1,6 @@
 #include "object_header.h"
 
+#include <numeric>
 #include <stdexcept>
 
 void ObjectHeaderMessage::Serialize(Serializer& s) const {
@@ -33,4 +34,53 @@ ObjectHeaderMessage ObjectHeaderMessage::Deserialize(Deserializer& de) {
     de.ReadBuffer(msg.message);
 
     return msg;
+}
+
+void ObjectHeader::Serialize(Serializer& s) const {
+    s.Write(kVersionNumber);
+    s.Write<uint8_t>(0);
+    s.Write(message_count);
+    s.Write(object_ref_count);
+    s.Write(object_header_size);
+    // reserved (zero)
+    s.Write<uint64_t>(0);
+
+    for (const ObjectHeaderMessage& msg: messages) {
+        s.WriteComplex(msg);
+    }
+}
+
+ObjectHeader ObjectHeader::Deserialize(Deserializer& de) {
+    if (de.Read<uint8_t>() != kVersionNumber) {
+        throw std::runtime_error("Version number was invalid");
+    }
+    // reserved (zero)
+    de.Skip<uint8_t>();
+
+    ObjectHeader hd{};
+
+    hd.message_count = de.Read<uint16_t>();
+    hd.object_ref_count = de.Read<uint64_t>();
+    hd.object_header_size = de.Read<uint64_t>();
+    // reserved (zero)
+    de.Skip<uint64_t>();
+
+    for (uint16_t m = 0; m < hd.message_count; ++m) {
+        hd.messages.push_back(de.ReadComplex<ObjectHeaderMessage>());
+    }
+
+    uint64_t total_bytes = std::reduce(
+        hd.messages.begin(),
+        hd.messages.end(),
+        static_cast<uint64_t>(0),
+        [](uint64_t acc, const ObjectHeaderMessage& msg) {
+            return acc + msg.size;
+        }
+    );
+
+    if (total_bytes != hd.object_header_size) {
+        throw std::runtime_error("Failed to read correct number of header bytes");
+    }
+
+    return hd;
 }
