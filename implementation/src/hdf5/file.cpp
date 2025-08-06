dd13372e184110ce7d66d6adba0ad6aced994cf2
abscosmos <joshua@dinoslice.com>
2025-08-06 15:00:35 -0500

    Error if superblock base address is not 0
    Eventually will need to change to support

--- implementation/src/hdf5/file.cpp
+++ implementation/src/hdf5/file.cpp
@@ -3,6 +3,10 @@
 File::File(const std::filesystem::path& path): read_(path) {
     superblock_ = read_.ReadComplex<SuperblockV0>();
 
+    if (superblock_.base_addr != 0) {
+        throw std::logic_error("not implemented");
+    }
+
     // read the root group
     offset_t root_group_header_addr = superblock_.root_group_symbol_table_entry_addr.object_header_addr;
     read_.SetPosition(superblock_.base_addr + root_group_header_addr);
