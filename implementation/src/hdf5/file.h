d994c5c8c99d6f9b01d8c5858b647c125cd63c6c
abscosmos <joshua@dinoslice.com>
2025-08-06 15:34:45 -0500

    Create method to test retrieving data from dataset

--- implementation/src/hdf5/file.h
+++ implementation/src/hdf5/file.h
@@ -11,6 +11,30 @@ class Dataset {
 public:
     explicit Dataset(const ObjectHeader& header, /* temporary */ Deserializer& de);
 
+    // TODO: multidimensional coords
+    template <typename T>
+    T Get(size_t index) const {
+        if (index >= space_.TotalElements()) {
+            throw std::out_of_range("Index out of bounds for dataset");
+        }
+
+        if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
+            throw std::logic_error("Variable length datatypes are not supported yet");
+        }
+
+        const auto* props = std::get_if<ContiguousStorageProperty>(&layout_.properties);
+
+        if (!props) {
+            throw std::logic_error("only contiguous storage currently supported");
+        }
+
+        size_t size = type_.Size();
+
+        read_.SetPosition(/* superblock.base_addr + */ props->address + index * size);
+
+        return read_.ReadComplex<T>();
+    }
+
 private:
     // TODO: handle references in a better way
     Deserializer& read_;
