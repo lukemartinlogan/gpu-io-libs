df1a909d66c8fbdf31073265836e27a253169432
abscosmos <joshua@dinoslice.com>
2025-08-16 15:07:08 -0500

    Ensure object header version is correct

--- implementation/src/hdf5/object.h
+++ implementation/src/hdf5/object.h
@@ -5,7 +5,14 @@ struct Object {
     explicit Object(ReaderWriter& io, offset_t pos_)
         : io_(io), file_pos_(pos_)
     {
+        JumpToRelativeOffset(0);
+
+        // FIXME: hardcoded constant
+        if (io_.Read<uint8_t>() != 0x01) {
+            throw std::runtime_error("Version number was invalid");
+        }
     }
+
 private:
     void JumpToRelativeOffset(offset_t offset) const {
         io_.SetPosition(file_pos_ + offset);
