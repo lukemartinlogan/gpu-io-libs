9efad7898f87a877b6372ea611e63077b4b2dabc
abscosmos <joshua@dinoslice.com>
2025-08-16 15:05:54 -0500

    Replace Deserializer in Object with ReaderWriter

--- implementation/src/hdf5/object.h
+++ implementation/src/hdf5/object.h
@@ -2,9 +2,11 @@
 #include "object_header.h"
 
 struct Object {
-    explicit Object(Deserializer& de, offset_t pos_)
-        : de_(de), file_pos_(pos_) {}
+    explicit Object(ReaderWriter& io, offset_t pos_)
+        : io_(io), file_pos_(pos_)
+    {
+    }
 private:
-    Deserializer& de_;
+    ReaderWriter& io_;
     offset_t file_pos_{};
 };
