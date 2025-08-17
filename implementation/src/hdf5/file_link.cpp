9f00c4438e617cf69c1e61d9baaad29882be6a68
abscosmos <joshua@dinoslice.com>
2025-08-17 11:44:01 -0500

    Only update superblock eof addr when allocating

--- implementation/src/hdf5/file_link.cpp
+++ implementation/src/hdf5/file_link.cpp
@@ -4,9 +4,11 @@ offset_t FileLink::AllocateAtEOF(len_t size_bytes) {
     offset_t addr = superblock.eof_addr;
     superblock.eof_addr += size_bytes;
 
-    // TODO: not necessary to write everything
-    io.SetPosition(superblock.base_addr);
-    io.WriteComplex(superblock);
+    // TODO: for some reason writing the whole superblock is invalid
+    // io.SetPosition(superblock.base_addr);
+    // io.WriteComplex(superblock);
+    io.SetPosition(superblock.base_addr + 40);
+    io.Write(superblock.eof_addr);
 
     io.SetPosition(addr);
 
