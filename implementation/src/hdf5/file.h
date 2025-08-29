58a51afd7fc1e0ed39027b7237925456c753c481
abscosmos <joshua@dinoslice.com>
2025-08-29 08:39:32 -0500

    In file, hold root group in an optional
    Otherwise, group's default ctor would have to be public, which would lead to invalid group instances

--- implementation/src/hdf5/file.h
+++ implementation/src/hdf5/file.h
@@ -9,12 +9,12 @@ public:
     explicit File(const std::filesystem::path& path);
 
     [[nodiscard]] Group RootGroup() {
-        return root_group_;
+        return *root_group_;
     }
 
 private:
     std::shared_ptr<FileLink> file_link_{};
 
     // root group
-    Group root_group_;
+    std::optional<Group> root_group_;
 };
