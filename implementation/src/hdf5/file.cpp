a7316ce3ccfb511a36b7ccb24b1d5779dc46f9d6
abscosmos <joshua@dinoslice.com>
2025-10-04 18:06:07 -0500

    Remove old root group check
    Used to check that root group had only one entry.

--- implementation/src/hdf5/file.cpp
+++ implementation/src/hdf5/file.cpp
@@ -19,11 +19,6 @@ hdf5::expected<File> File::New(const std::filesystem::path& path) {
     // read the root group
     offset_t root_group_header_addr = file_link->superblock.base_addr + file_link->superblock.root_group_symbol_table_entry_addr.object_header_addr;
 
-    // FIXME: bring back this check?
-    // if (root_header.messages.size() != 1) {
-    //     return hdf5::error(hdf5::HDF5ErrorCode::InvalidDataValue, "Root group must have exactly one message");
-    // }
-
     auto root_group = Group::New(Object(file_link, root_group_header_addr));
     if (!root_group) {
         return cstd::unexpected(root_group.error());
