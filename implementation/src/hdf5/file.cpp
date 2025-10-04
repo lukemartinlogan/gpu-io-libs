f5916bee8716cafeabf2fbb3b87099c906fa9f0e
abscosmos <joshua@dinoslice.com>
2025-10-04 18:04:45 -0500

    Refactor file.h & file.cpp to use expected

--- implementation/src/hdf5/file.cpp
+++ implementation/src/hdf5/file.cpp
@@ -1,7 +1,8 @@
 #include "file.h"
 
-File::File(const std::filesystem::path& path)
-{
+hdf5::expected<File> File::New(const std::filesystem::path& path) {
+    std::shared_ptr<FileLink> file_link;
+
     // scope since file_io and superblock are invalid after make_shared
     {
         StdioReaderWriter file_io(path);
@@ -9,19 +10,24 @@ File::File(const std::filesystem::path& path)
         auto superblock = file_io.ReadComplex<SuperblockV0>();
 
         if (superblock.base_addr != 0) {
-            throw std::logic_error("not implemented");
+            return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "non-zero base address not implemented");
         }
 
-        file_link_ = std::make_shared<FileLink>(std::move(file_io), superblock);
+        file_link = std::make_shared<FileLink>(std::move(file_io), superblock);
     }
 
     // read the root group
-    offset_t root_group_header_addr = file_link_->superblock.base_addr + file_link_->superblock.root_group_symbol_table_entry_addr.object_header_addr;
+    offset_t root_group_header_addr = file_link->superblock.base_addr + file_link->superblock.root_group_symbol_table_entry_addr.object_header_addr;
 
     // FIXME: bring back this check?
     // if (root_header.messages.size() != 1) {
-    //     throw std::runtime_error("Root group must have exactly one message");
+    //     return hdf5::error(hdf5::HDF5ErrorCode::InvalidDataValue, "Root group must have exactly one message");
     // }
 
-    root_group_ = Group(Object(file_link_, root_group_header_addr));
+    auto root_group = Group::New(Object(file_link, root_group_header_addr));
+    if (!root_group) {
+        return cstd::unexpected(root_group.error());
+    }
+
+    return File(std::move(file_link), std::move(*root_group));
 }
--- implementation/src/hdf5/file.h
+++ implementation/src/hdf5/file.h
@@ -7,13 +7,16 @@
 
 class File {
 public:
-    explicit File(const std::filesystem::path& path);
+    static hdf5::expected<File> New(const std::filesystem::path& path);
 
     [[nodiscard]] Group RootGroup() {
         return *root_group_;
     }
 
 private:
+    File(std::shared_ptr<FileLink> file_link, Group root_group)
+        : file_link_(std::move(file_link)), root_group_(std::move(root_group)) {}
+
     std::shared_ptr<FileLink> file_link_{};
 
     // root group
