353093c1667d3a2d3066f6ae6e5ea2ac0f5a6061
abscosmos <joshua@dinoslice.com>
2025-11-08 15:13:06 -0600

    Move hdf5 errors & expected to separate header file
    Eliminates circular dependencies with gpu_string

--- implementation/src/hdf5/types.h
+++ implementation/src/hdf5/types.h
@@ -38,72 +38,8 @@ namespace hdf5 {
     }
 #endif
 
+// Include error types (requires cstd namespace to be defined)
+#include "error.h"
+
 namespace hdf5 {
-    enum class HDF5ErrorCode {
-        // File I/O Errors
-        FileOpenFailed,
-        FileSeekFailed,
-        FilePositionFailed,
-
-        // Format/Validation Errors
-        InvalidSignature,
-        InvalidVersion,
-        InvalidChecksum,
-        InvalidType,
-        InvalidClass,
-        InvalidFlags,
-        InvalidTerminator,
-        SizeMismatch,
-
-        // Bounds/Range Errors
-        IndexOutOfBounds,
-        CoordinateOutOfBounds,
-        SelectionOutOfBounds,
-        SelectionOverflow,
-        IteratorAtEnd,
-
-        // Invalid Arguments
-        BufferTooSmall,
-        BufferTooLarge,
-        EmptyParameter,
-        ZeroParameter,
-        DimensionMismatch,
-        BlockOverlap,
-        InvalidParameterCombination,
-
-        // Not Implemented
-        NotImplemented,
-        FeatureNotSupported,
-        DeprecatedFeature,
-
-        // Logic Errors (Internal consistency/state errors)
-        InvalidVariantState,
-        WrongNodeType,
-        EmptyNode,
-        CapacityExceeded,
-        AllocationMismatch,
-        FieldPresenceMismatch,
-
-        // Data Errors
-        StringNotNullTerminated,
-        IncorrectByteCount,
-        InvalidDataValue,
-
-        // Resource/Limit Errors
-        BTreeOverflow,
-        MaxDimensionsExceeded,
-        BufferNotAligned
-    };
-
-    struct HDF5Error {
-        HDF5ErrorCode code;
-        const char* description;
-    };
-
-    template<typename T>
-    using expected = cstd::expected<T, HDF5Error>;
-
-    constexpr cstd::unexpected<HDF5Error> error(const HDF5ErrorCode code, const char* desc = nullptr) {
-        return cstd::unexpected(HDF5Error{ .code = code, .description = desc });
-    }
 }
