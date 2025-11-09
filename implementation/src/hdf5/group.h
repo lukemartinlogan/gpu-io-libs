faf4c40d1f927b34f61fb957cdf2bc8f4d7d639b
abscosmos <joshua@dinoslice.com>
2025-11-08 20:12:08 -0600

    Convert group to use hdf5::string

--- implementation/src/hdf5/group.h
+++ implementation/src/hdf5/group.h
@@ -7,29 +7,30 @@
 #include "tree.h"
 #include "file_link.h"
 #include "object.h"
+#include "gpu_string.h"
 
 class Group {
 public:
     static hdf5::expected<Group> New(const Object& object);
 
-    [[nodiscard]] hdf5::expected<Dataset> OpenDataset(std::string_view dataset_name) const;
+    [[nodiscard]] hdf5::expected<Dataset> OpenDataset(hdf5::string_view dataset_name) const;
 
     hdf5::expected<Dataset> CreateDataset(
-        std::string_view dataset_name,
+        hdf5::string_view dataset_name,
         const hdf5::dim_vector<len_t>& dimension_sizes,
         const DatatypeMessage& type,
         cstd::optional<hdf5::dim_vector<uint32_t>> chunk_dims = cstd::nullopt,
         cstd::optional<std::vector<byte_t>> fill_value = cstd::nullopt
     );
 
-    [[nodiscard]] hdf5::expected<Group> OpenGroup(std::string_view group_name) const;
+    [[nodiscard]] hdf5::expected<Group> OpenGroup(hdf5::string_view group_name) const;
 
-    hdf5::expected<Group> CreateGroup(std::string_view name);
+    hdf5::expected<Group> CreateGroup(hdf5::string_view name);
 
-    [[nodiscard]] hdf5::expected<cstd::optional<Object>> Get(std::string_view name) const;
+    [[nodiscard]] hdf5::expected<cstd::optional<Object>> Get(hdf5::string_view name) const;
 
 private:
-    hdf5::expected<void> Insert(std::string_view name, offset_t object_header_ptr);
+    hdf5::expected<void> Insert(hdf5::string_view name, offset_t object_header_ptr);
 
     // FIXME: get rid of this method
     [[nodiscard]] const LocalHeap& GetLocalHeap() const {
