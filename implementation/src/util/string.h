71d1206aff2cd36a195a7060c4f0e34a597cb345
abscosmos <joshua@dinoslice.com>
2025-11-22 10:47:19 -0600

    Add bounds checking to ReadNullTerminatedString

--- implementation/src/util/string.h
+++ implementation/src/util/string.h
@@ -3,17 +3,25 @@
 #include "../serialization/serialization.h"
 #include "../hdf5/types.h"
 
-inline hdf5::string ReadNullTerminatedString(Deserializer& de, cstd::optional<size_t> max_size) {
+inline hdf5::expected<hdf5::string> ReadNullTerminatedString(Deserializer& de, size_t max_size = hdf5::gpu_string<>::max_size()) {
     hdf5::string str;
+    bool found = false;
 
-    while (!max_size || str.size() < *max_size) {
+    ASSERT(max_size <= hdf5::gpu_string<>::max_size(), "max_size must be less than inplace string max size");
+
+    while (str.size() < max_size) {
         auto c = static_cast<char>(de.Read<byte_t>());
 
         if (c == '\0') {
+            found = true;
             break;
         }
 
         str.push_back(c);
+    }
+
+    if (!found) {
+        return hdf5::error(hdf5::HDF5ErrorCode::StringNotNullTerminated, "string read was not null-terminated");
     }
 
     return str;
