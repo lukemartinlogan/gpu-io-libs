1b48fe0d74a7b4248777cc451429b1693684ce4f
abscosmos <joshua@dinoslice.com>
2025-11-22 19:08:34 -0600

    Create {Read, Write}PaddedString methods in string.h

--- implementation/src/util/string.h
+++ implementation/src/util/string.h
@@ -3,14 +3,15 @@
 #include "../serialization/serialization.h"
 #include "../hdf5/types.h"
 
-inline hdf5::expected<hdf5::string> ReadNullTerminatedString(VirtualDeserializer& de, size_t max_size = hdf5::gpu_string<>::max_size()) {
+template<serde::Deserializer D>
+hdf5::expected<hdf5::string> ReadNullTerminatedString(D& de, size_t max_size = hdf5::gpu_string<>::max_size()) {
     hdf5::string str;
     bool found = false;
 
     ASSERT(max_size <= hdf5::gpu_string<>::max_size(), "max_size must be less than inplace string max size");
 
     while (str.size() < max_size) {
-        auto c = static_cast<char>(de.Read<byte_t>());
+        auto c = static_cast<char>(serde::Read<byte_t>(de));
 
         if (c == '\0') {
             found = true;
@@ -25,4 +26,53 @@ inline hdf5::expected<hdf5::string> ReadNullTerminatedString(VirtualDeserializer
     }
 
     return str;
+}
+
+// TODO: does this forget about the null byte sometimes?
+template<serde::Serializer S>
+void WritePaddedString(hdf5::string_view name, S& s) {
+    size_t name_size = name.size();
+
+    // write string
+    s.WriteBuffer(std::span(
+        reinterpret_cast<const byte_t*>(name.data()),
+        name_size
+    ));
+
+    // pad to 8 bytes
+    size_t padding = (name_size / 8 + 1) * 8 - name_size;
+    static constexpr cstd::array<byte_t, 8> nul_bytes{};
+
+    s.WriteBuffer(std::span(nul_bytes.data(), padding));
+}
+
+template<serde::Deserializer D>
+hdf5::expected<hdf5::string> ReadPaddedString(D& de) {
+    hdf5::string name;
+
+    for (;;) {
+        // 8 byte blocks
+        cstd::array<byte_t, 8> buf{};
+
+        if (!de.ReadBuffer(buf)) {
+            return hdf5::error(hdf5::HDF5ErrorCode::BufferTooSmall, "failed to read string block");
+        }
+
+        auto nul_pos = std::ranges::find(buf, static_cast<byte_t>('\0'));
+
+        auto append_result = name.append(hdf5::string_view(
+            reinterpret_cast<const char*>(buf.data()),
+            std::distance(buf.begin(), nul_pos)
+        ));
+
+        if (!append_result) {
+            return cstd::unexpected(append_result.error());
+        }
+
+        if (nul_pos != buf.end()) {
+            break;
+        }
+    }
+
+    return name;
 }
