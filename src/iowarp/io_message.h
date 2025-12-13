008cf489c1a990c5fffe7ba989466d2314e6d86d
abscosmos <joshua@dinoslice.com>
2025-12-13 11:36:52 -0600

    Add factory methods for creating IoMessages

--- src/iowarp/io_message.h
+++ src/iowarp/io_message.h
@@ -27,6 +27,54 @@ struct IoMessage {
   int fd;             // output for kOpen, input for kRead/kWrite/kClose
   int32_t result_;    // return value from syscall (bytes read/written, or -1)
   int errno_;         // error code if result_ < 0
+
+  // factory methods
+  __device__ __host__
+  static IoMessage Open(char* filename, int flags, int mode) {
+    return IoMessage{
+      .type_ = IoType::kOpen,
+      .filename = filename,
+      .flags = flags,
+      .mode = mode
+    };
+  }
+
+  __device__ __host__
+  static IoMessage Write(int fd, char* buffer, size_t size, size_t offset) {
+    return IoMessage{
+      .type_ = IoType::kWrite,
+      .buffer = buffer,
+      .size = size,
+      .offset = offset,
+      .fd = fd
+    };
+  }
+
+  __device__ __host__
+  static IoMessage Read(int fd, char* buffer, size_t size, size_t offset) {
+    return IoMessage{
+      .type_ = IoType::kRead,
+      .buffer = buffer,
+      .size = size,
+      .offset = offset,
+      .fd = fd
+    };
+  }
+
+  __device__ __host__
+  static IoMessage Close(int fd) {
+    return IoMessage{
+      .type_ = IoType::kClose,
+      .fd = fd
+    };
+  }
+
+  __device__ __host__
+  static IoMessage Shutdown() {
+    return IoMessage{
+      .type_ = IoType::kShutdown
+    };
+  }
 };
 
 } // namespace iowarp
