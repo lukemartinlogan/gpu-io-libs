0cd68706145fab606693466b9197f8aebd698cae
abscosmos <joshua@dinoslice.com>
2025-12-13 11:51:42 -0600

    Add GpuContext to track files

---
+++ src/iowarp/gpu_context.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "../hdf5/cstd.h"
+#include "shm_queue.h"
+#include "../hdf5/types.h"
+
+namespace iowarp {
+
+struct FileEntry {
+  int fd;
+  cstd::atomic<offset_t> eof_offset;
+  bool is_open;
+
+  __device__ __host__
+  FileEntry() : fd(-1), eof_offset(0), is_open(false) {}
+};
+
+// GPU execution context for I/O operations
+// Simplified to track only one file at a time
+struct GpuContext {
+  shm_queue* queue_;
+  FileEntry file_entry_;
+
+  __device__ __host__
+  GpuContext() : queue_(nullptr) {}
+
+  __device__ __host__
+  [[nodiscard]] offset_t GetEOF(int fd) const {
+    if (file_entry_.fd != fd || !file_entry_.is_open) return 0;
+    return file_entry_.eof_offset.load(cstd::memory_order_acquire);
+  }
+
+  __device__ __host__
+  void UpdateEOF(int fd, size_t new_eof) {
+    if (file_entry_.fd != fd || !file_entry_.is_open) return;
+
+    offset_t current_eof = file_entry_.eof_offset.load(cstd::memory_order_acquire);
+    while (new_eof > current_eof) {
+      if (file_entry_.eof_offset.compare_exchange_weak(
+          current_eof, new_eof,
+          cstd::memory_order_release,
+          cstd::memory_order_acquire
+        )
+      ) {
+        break;
+      }
+    }
+  }
+};
+
+} // namespace iowarp
