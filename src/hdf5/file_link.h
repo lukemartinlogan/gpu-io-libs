ae2e9f3a8a8fede7e6c80127253f422d7f04bd20
abscosmos <joshua@dinoslice.com>
2025-12-14 14:55:21 -0600

    Have AllocateAtEOF take in a serializer

--- src/hdf5/file_link.h
+++ src/hdf5/file_link.h
@@ -2,6 +2,7 @@
 
 #include "superblock.h"
 #include "../serialization/stdio.h"
+#include "../serialization/serialization.h"
 
 // FIXME: struct name
 struct FileLink {
@@ -12,6 +13,19 @@ struct FileLink {
     FileLink(StdioReaderWriter file_io, const SuperblockV0& superblock)
         : io(std::move(file_io)), superblock(superblock) {}
 
+    template<serde::Serializer S>
     __device__ __host__
-    offset_t AllocateAtEOF(len_t size_bytes);
+    offset_t AllocateAtEOF(len_t size_bytes, S& serializer) {
+        offset_t addr = superblock.eof_addr;
+        superblock.eof_addr += size_bytes;
+
+        // Write the new EOF address to the superblock in the file
+        // (offset 40 is where eof_addr is stored in the superblock)
+        serializer.SetPosition(superblock.base_addr + 40);
+        serde::Write(serializer, superblock.eof_addr);
+
+        serializer.SetPosition(addr);
+
+        return addr;
+    }
 };
