7a7d7fd1d08959b87fda002f33c8c513dc2d71ba
abscosmos <joshua@dinoslice.com>
2025-12-13 21:56:40 -0600

    Remove debug printing

--- src/iowarp/cpu_polling.cpp
+++ src/iowarp/cpu_polling.cpp
@@ -17,12 +17,10 @@ PollingThreadManager::PollingThreadManager(shm_queue* queue, GpuContext* ctx)
   , ctx_(ctx)
   , thread_([this](std::stop_token st) { Poll(st); })
 {
-  std::cout << "[CPU] Starting polling thread" << std::endl;
 }
 
 void PollingThreadManager::Stop() {
   if (thread_.joinable()) {
-    std::cout << "[CPU] Requesting stop" << std::endl;
     thread_.request_stop();
 
     if (queue_->size() == 0) {
@@ -32,8 +30,6 @@ void PollingThreadManager::Stop() {
 }
 
 void PollingThreadManager::Poll(std::stop_token stop_token) {
-  std::cout << "[CPU] Polling thread started" << std::endl;
-
   while (!stop_token.stop_requested()) {
     uint64_t qsize = queue_->size();
     if (qsize == 0) {
@@ -41,16 +37,10 @@ void PollingThreadManager::Poll(std::stop_token stop_token) {
       continue;
     }
 
-    std::cout << "[CPU] Got message, size=" << qsize << std::endl;
-
     auto& msg = queue_->get();
-    std::cout << "[CPU] Got message, type=" << static_cast<int>(msg.type_) << std::endl;
 
     switch (msg.type_) {
       case IoType::kOpen: {
-        std::cout << "[CPU] Opening file: " << msg.filename
-                  << " (flags=" << msg.flags << ", mode=" << msg.mode << ")" << std::endl;
-
         int fd;
 #ifdef _WIN32
         fd = _open(msg.filename, msg.flags, msg.mode);
@@ -60,16 +50,10 @@ void PollingThreadManager::Poll(std::stop_token stop_token) {
         msg.fd = fd;
         msg.result_ = fd >= 0 ? 0 : -1;
         msg.errno_ = fd >= 0 ? 0 : errno;
-
-        std::cout << "[CPU] Open result: fd=" << fd << std::endl;
         break;
       }
 
       case IoType::kWrite: {
-        std::cout << "[CPU] Writing " << msg.size << " bytes at offset "
-                  << msg.offset << " to fd=" << msg.fd
-                  << " (buffer=" << (void*)msg.buffer << ")" << std::endl;
-
         ssize_t written;
 #ifdef _WIN32
         _lseeki64(msg.fd, msg.offset, SEEK_SET);
@@ -79,17 +63,10 @@ void PollingThreadManager::Poll(std::stop_token stop_token) {
 #endif
         msg.result_ = written;
         msg.errno_ = written >= 0 ? 0 : errno;
-
-        std::cout << "[CPU] Write result: " << written << " bytes";
-        if (written < 0) std::cout << " (errno=" << errno << ")";
-        std::cout << std::endl;
         break;
       }
 
       case IoType::kRead: {
-        std::cout << "[CPU] Reading " << msg.size << " bytes at offset "
-                  << msg.offset << " from fd=" << msg.fd << std::endl;
-
         ssize_t read_bytes;
 #ifdef _WIN32
         _lseeki64(msg.fd, msg.offset, SEEK_SET);
@@ -99,16 +76,10 @@ void PollingThreadManager::Poll(std::stop_token stop_token) {
 #endif
         msg.result_ = read_bytes;
         msg.errno_ = read_bytes >= 0 ? 0 : errno;
-
-        std::cout << "[CPU] Read result: " << read_bytes << " bytes";
-        if (read_bytes < 0) std::cout << " (errno=" << errno << ")";
-        std::cout << std::endl;
         break;
       }
 
       case IoType::kClose: {
-        std::cout << "[CPU] Closing fd=" << msg.fd << std::endl;
-
         int result;
 #ifdef _WIN32
         result = _close(msg.fd);
@@ -117,26 +88,20 @@ void PollingThreadManager::Poll(std::stop_token stop_token) {
 #endif
         msg.result_ = result;
         msg.errno_ = result >= 0 ? 0 : errno;
-
-        std::cout << "[CPU] Close result: " << result << std::endl;
         break;
       }
 
       case IoType::kShutdown: {
-        std::cout << "[CPU] Shutdown requested" << std::endl;
         queue_->clear();
         return;
       }
 
       default:
-        std::cout << "[CPU] Unknown message type: " << static_cast<int>(msg.type_) << std::endl;
         break;
     }
 
     queue_->clear();
   }
-
-  std::cout << "[CPU] Polling thread exiting" << std::endl;
 }
 
 } // namespace iowarp
