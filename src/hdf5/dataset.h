36184219c05acf38a4a0bbf3f50dfc6cd143e0cb
abscosmos <joshua@dinoslice.com>
2025-12-19 18:59:19 -0600

    Implement dataset methods outside of class definition
    Still implemented in the header file, however.

--- src/hdf5/dataset.h
+++ src/hdf5/dataset.h
@@ -15,54 +15,11 @@ public:
     // TODO: multidimensional coords
     __device__
     template <typename T>
-    hdf5::expected<T> Get(size_t index) const {
-        if (index >= space_.TotalElements()) {
-            return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index out of bounds for dataset");
-        }
-
-        if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
-            return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Variable length datatypes are not supported yet");
-        }
-
-        const auto* props = cstd::get_if<ContiguousStorageProperty>(&layout_.properties);
-
-        if (!props) {
-            return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "only contiguous storage currently supported");
-        }
-
-        size_t size = type_.Size();
-
-        auto io = object_.file->MakeRW();
-        io.SetPosition(object_.file->superblock.base_addr + props->address + index * size);
-
-        return serde::Read<T>(io);
-    }
+    hdf5::expected<T> Get(size_t index) const;
 
     // FIXME: implement datatype
     __device__
     hdf5::expected<void> Read(cstd::span<byte_t> buffer, size_t start_index, size_t count) const;
-
-    // FIXME: impl datatype instead
-    __host__
-    template<typename T>
-    hdf5::expected<std::vector<T>> Read(size_t start_index, size_t count) const {
-        std::vector<T> out(count);
-
-        hdf5::expected<void> result = Read(
-            cstd::span(
-                reinterpret_cast<byte_t*>(out.data()),
-                out.size() * sizeof(T)
-            ),
-            start_index,
-            count
-        );
-
-        if (!result) {
-            return cstd::unexpected(result.error());
-        }
-
-        return out;
-    }
 
     __device__
     hdf5::expected<void> ReadHyperslab(
@@ -73,50 +30,12 @@ public:
         const hdf5::dim_vector<uint64_t>& block = {}
     ) const;
 
-    __host__
-    template<typename T>
-    hdf5::expected<std::vector<T>> ReadHyperslab(
-        const hdf5::dim_vector<uint64_t>& start,
-        const hdf5::dim_vector<uint64_t>& count,
-        const hdf5::dim_vector<uint64_t>& stride = {},
-        const hdf5::dim_vector<uint64_t>& block = {}
-    ) const {
-        size_t total_elements = TotalElements(count, block);
-
-        std::vector<T> result(total_elements);
-
-        hdf5::expected<void> res = ReadHyperslab(
-            cstd::span(
-                reinterpret_cast<byte_t*>(result.data()),
-                result.size() * sizeof(T)
-            ),
-            start,
-            count,
-            stride,
-            block
-        );
-
-        if (!res) {
-            return cstd::unexpected(res.error());
-        }
-
-        return result;
-    }
-
     __device__
     hdf5::expected<void> Write(cstd::span<const byte_t> data, size_t start_index) const;
 
     __device__
     template<typename T>
-    hdf5::expected<void> Write(cstd::span<const T> data, size_t start_index) const {
-        return Write(
-            cstd::span(
-                reinterpret_cast<const byte_t*>(data.data()),
-                data.size_bytes()
-            ),
-            start_index
-        );
-    }
+    hdf5::expected<void> Write(cstd::span<const T> data, size_t start_index) const;
 
     __device__
     hdf5::expected<void> WriteHyperslab(
@@ -172,3 +91,61 @@ private:
     DatatypeMessage type_{};
     DataspaceMessage space_{};
 };
+
+template <typename T>
+__device__
+inline hdf5::expected<T> Dataset::Get(size_t index) const {
+    if (index >= space_.TotalElements()) {
+        return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index out of bounds for dataset");
+    }
+
+    if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Variable length datatypes are not supported yet");
+    }
+
+    const auto* props = cstd::get_if<ContiguousStorageProperty>(&layout_.properties);
+
+    if (!props) {
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "only contiguous storage currently supported");
+    }
+
+    size_t size = type_.Size();
+
+    auto io = object_.file->MakeRW();
+    io.SetPosition(object_.file->superblock.base_addr + props->address + index * size);
+
+    return serde::Read<T>(io);
+}
+
+template<typename T>
+__device__
+inline hdf5::expected<void> Dataset::Write(cstd::span<const T> data, size_t start_index) const {
+    return Write(
+        cstd::span(
+            reinterpret_cast<const byte_t*>(data.data()),
+            data.size_bytes()
+        ),
+        start_index
+    );
+}
+
+template<typename T>
+__device__
+inline hdf5::expected<void> Dataset::WriteHyperslab(
+    cstd::span<const T> data,
+    const hdf5::dim_vector<uint64_t>& start,
+    const hdf5::dim_vector<uint64_t>& count,
+    const hdf5::dim_vector<uint64_t>& stride,
+    const hdf5::dim_vector<uint64_t>& block
+) const {
+    return WriteHyperslab(
+        cstd::span(
+            reinterpret_cast<const byte_t*>(data.data()),
+            data.size_bytes()
+        ),
+        start,
+        count,
+        stride,
+        block
+    );
+}
