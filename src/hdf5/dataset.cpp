b9ae71e84a70bcdc0ec9cf224c3ea68dd89141de
abscosmos <joshua@dinoslice.com>
2026-01-12 01:26:53 +0000

    Restore stubbed dataset.cpp methods

--- src/hdf5/dataset.cpp
+++ src/hdf5/dataset.cpp
@@ -1,20 +1,198 @@
-// TODO(kernel-hang): TEMPORARILY STUBBED to fix GPU kernel hang
-
 #include "dataset.h"
+#include "hyperslab.h"
 
 __device__
 hdf5::expected<Dataset> Dataset::New(const Object& object) {
-    return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "Dataset::New temporarily disabled");
+    auto header_result = object.GetHeader();
+    if (!header_result) return cstd::unexpected(header_result.error());
+
+    bool found_layout = false, found_type = false, found_space = false;
+    DataLayoutMessage layout{};
+    DatatypeMessage type{};
+    DataspaceMessage space{};
+
+    for (const ObjectHeaderMessage& msg: header_result->messages) {
+        if (const auto* layout_ptr = cstd::get_if<DataLayoutMessage>(&msg.message)) {
+            layout = *layout_ptr;
+            found_layout = true;
+        }
+        else if (const auto* type_ptr = cstd::get_if<DatatypeMessage>(&msg.message)) {
+            type = *type_ptr;
+            found_type = true;
+        }
+        else if (const auto* space_ptr = cstd::get_if<DataspaceMessage>(&msg.message)) {
+            space = *space_ptr;
+            found_space = true;
+        }
+    }
+
+    if (!found_layout || !found_type || !found_space) {
+        return hdf5::error(hdf5::HDF5ErrorCode::InvalidDataValue, "Dataset header does not contain all required messages");
+    }
+
+    return Dataset(object, layout, type, space);
 }
 
 __device__
 hdf5::expected<void> Dataset::Read(cstd::span<byte_t> buffer, size_t start_index, size_t count) const {
-    return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "Dataset::Read temporarily disabled");
+    if (start_index + count > space_.TotalElements()) {
+        return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index range out of bounds for dataset");
+    }
+
+    size_t element_size = type_.Size();
+    size_t total_bytes = count * element_size;
+
+    if (buffer.size() < total_bytes) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferTooSmall, "Buffer too small for requested data");
+    }
+    if (buffer.size() > total_bytes) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferTooLarge, "Buffer size exceeds requested data size");
+    }
+
+    if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Variable length datatypes are not supported yet");
+    }
+
+    auto props = layout_.properties;
+
+    if (const auto* compact = cstd::get_if<CompactStorageProperty>(&props)) {
+        auto start = compact->raw_data.begin() + static_cast<ptrdiff_t>(start_index * element_size);
+
+        if (start + static_cast<ptrdiff_t>(total_bytes) > compact->raw_data.end()) {
+            return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index range out of bounds for compact storage dataset");
+        }
+
+        cstd::_copy(start, start + total_bytes, buffer.data());
+
+    } else if (const auto* contiguous = cstd::get_if<ContiguousStorageProperty>(&props)) {
+        if ((start_index + count) * element_size > contiguous->size) {
+            return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index range out of bounds for contiguous storage dataset");
+        }
+
+        auto io = object_.file->MakeRW();
+        io.SetPosition(contiguous->address + start_index * element_size);
+        io.ReadBuffer(cstd::span(buffer.data(), total_bytes));
+
+    } else if (const auto* chunked = cstd::get_if<ChunkedStorageProperty>(&props)) {
+        return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "chunked read not implemented yet");
+    } else {
+        return hdf5::error(hdf5::HDF5ErrorCode::InvalidVariantState, "unknown storage type in dataset");
+    }
+
+    return {};
 }
 
 __device__
 hdf5::expected<void> Dataset::Write(cstd::span<const byte_t> data, size_t start_index) const {
-    return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "Dataset::Write temporarily disabled");
+    if (data.size() % type_.Size() != 0) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferNotAligned, "Buffer size must be a multiple of the datatype size");
+    }
+
+    size_t count = data.size() / type_.Size();
+
+    if (start_index + count > space_.TotalElements()) {
+        return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index range out of bounds for dataset");
+    }
+
+    size_t element_size = type_.Size();
+
+    if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Variable length datatypes are not supported yet");
+    }
+
+    auto props = layout_.properties;
+
+    if (auto* compact = cstd::get_if<CompactStorageProperty>(&props)) {
+        auto start = compact->raw_data.begin() + static_cast<ptrdiff_t>(start_index * element_size);
+
+        if (start + static_cast<ptrdiff_t>(data.size()) > compact->raw_data.end()) {
+            return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index range out of bounds for compact storage dataset");
+        }
+
+        cstd::_copy(data.begin(), data.end(), start);
+
+    } else if (const auto* contiguous = cstd::get_if<ContiguousStorageProperty>(&props)) {
+        if ((start_index + count) * element_size > contiguous->size) {
+            return hdf5::error(hdf5::HDF5ErrorCode::IndexOutOfBounds, "Index range out of bounds for contiguous storage dataset");
+        }
+
+        auto io = object_.file->MakeRW();
+        io.SetPosition(contiguous->address + start_index * element_size);
+        io.WriteBuffer(data);
+
+    } else if (const auto* chunked = cstd::get_if<ChunkedStorageProperty>(&props)) {
+        return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "chunked write not implemented yet");
+    } else {
+        return hdf5::error(hdf5::HDF5ErrorCode::InvalidVariantState, "unknown storage type in dataset");
+    }
+
+    return {};
+}
+
+
+template<typename Visitor>
+__device__ hdf5::expected<void> ProcessChunkedHyperslab(
+    const ChunkedStorageProperty* chunked,
+    HyperslabIterator& iterator,
+    size_t element_size,
+    FileLink* file,
+    Visitor&& visitor
+) {
+    ChunkedBTree chunked_tree(
+        chunked->b_tree_addr,
+        file,
+        {
+            .dimensionality = static_cast<uint8_t>(chunked->dimension_sizes.size()),
+            .elem_byte_size = element_size,
+        }
+    );
+
+    size_t buffer_offset = 0;
+
+    while (!iterator.IsAtEnd()) {
+        const auto& current_coord = iterator.GetCurrentCoordinate();
+
+        ChunkCoordinates chunk_coords;
+        chunk_coords.coords.resize(chunked->dimension_sizes.size());
+
+        uint64_t within_chunk_offset = 0;
+        uint64_t chunk_stride = 1;
+
+        // process dimensions in reverse order
+        for (int dim = static_cast<int>(chunked->dimension_sizes.size()) - 1; dim >= 0; --dim) {
+            uint64_t chunk_size = chunked->dimension_sizes[dim];
+            uint64_t coord = current_coord[dim];
+
+            uint64_t chunk_offset = (coord / chunk_size) * chunk_size;
+            uint64_t within_chunk = coord % chunk_size;
+
+            // calculate linear offset
+            within_chunk_offset += within_chunk * chunk_stride;
+            chunk_stride *= chunk_size;
+
+            chunk_coords.coords[dim] = chunk_offset;
+        }
+
+        auto chunk_file_offset_result = chunked_tree.GetChunk(chunk_coords);
+        if (!chunk_file_offset_result) {
+            return cstd::unexpected(chunk_file_offset_result.error());
+        }
+
+        cstd::optional<offset_t> element_file_offset;
+        if (chunk_file_offset_result->has_value()) {
+            element_file_offset = **chunk_file_offset_result + within_chunk_offset * element_size;
+        }
+
+        auto visitor_result = visitor(element_file_offset, buffer_offset, chunk_coords);
+        if (!visitor_result) {
+            return cstd::unexpected(visitor_result.error());
+        }
+
+        buffer_offset += element_size;
+        iterator.Advance();
+    }
+
+    return {};
 }
 
 __device__
@@ -25,7 +203,111 @@ hdf5::expected<void> Dataset::ReadHyperslab(
     const hdf5::dim_vector<uint64_t>& stride,
     const hdf5::dim_vector<uint64_t>& block
 ) const {
-    return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "Dataset::ReadHyperslab temporarily disabled");
+    if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Variable length datatypes are not supported yet");
+    }
+
+    hdf5::dim_vector<uint64_t> dataset_dims(space_.dimensions.size());
+
+    cstd::transform(
+        space_.dimensions.begin(),
+        space_.dimensions.end(),
+        dataset_dims.begin(),
+        [](const auto& dim_info) { return dim_info.size; }
+    );
+
+    auto iterator_result = HyperslabIterator::New(start, count, stride, block, dataset_dims);
+    if (!iterator_result) {
+        return cstd::unexpected(iterator_result.error());
+    }
+    HyperslabIterator iterator = cstd::move(*iterator_result);
+
+    size_t element_size = type_.Size();
+    auto total_elements_result = iterator.GetTotalElements();
+
+    if (!total_elements_result) {
+        return cstd::unexpected(total_elements_result.error());
+    }
+
+    uint64_t total_elements = *total_elements_result;
+
+    if (buffer.size() < total_elements * element_size) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferTooSmall, "Buffer too small for hyperslab data");
+    }
+    if (buffer.size() > total_elements * element_size) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferTooLarge, "Buffer size exceeds hyperslab data size");
+    }
+
+    auto props = layout_.properties;
+
+    if (const auto* compact = cstd::get_if<CompactStorageProperty>(&props)) {
+        size_t buffer_offset = 0;
+
+        while (!iterator.IsAtEnd()) {
+            auto linear_index_result = iterator.GetLinearIndex();
+
+            if (!linear_index_result) {
+                return cstd::unexpected(linear_index_result.error());
+            }
+
+            size_t data_offset = *linear_index_result * element_size;
+
+            if (data_offset + element_size > compact->raw_data.size()) {
+                return hdf5::error(hdf5::HDF5ErrorCode::SelectionOutOfBounds, "Hyperslab selection exceeds compact storage bounds");
+            }
+
+            cstd::_copy(
+                compact->raw_data.begin() + data_offset,
+                compact->raw_data.begin() + data_offset + element_size,
+                buffer.data() + buffer_offset
+            );
+
+            buffer_offset += element_size;
+            iterator.Advance();
+        }
+
+    } else if (const auto* contiguous = cstd::get_if<ContiguousStorageProperty>(&props)) {
+        size_t buffer_offset = 0;
+        auto io = object_.file->MakeRW();
+
+        while (!iterator.IsAtEnd()) {
+            auto linear_index = iterator.GetLinearIndex();
+
+            if (!linear_index) {
+                return cstd::unexpected(linear_index.error());
+            }
+
+            offset_t file_offset = contiguous->address + *linear_index * element_size;
+
+            io.SetPosition(file_offset);
+            io.ReadBuffer(cstd::span(buffer.data() + buffer_offset, element_size));
+
+            buffer_offset += element_size;
+            iterator.Advance();
+        }
+
+    } else if (const auto* chunked = cstd::get_if<ChunkedStorageProperty>(&props)) {
+        auto io = object_.file->MakeRW();
+        auto process_result = ProcessChunkedHyperslab(
+            chunked, iterator, element_size, object_.file,
+            [&](const cstd::optional<offset_t>& element_file_offset, size_t buffer_offset, const ChunkCoordinates& /* chunk_coords */) -> hdf5::expected<void> {
+                if (!element_file_offset.has_value()) {
+                    // chunk doesn't exist (sparse dataset)
+                    cstd::fill_n(buffer.data() + buffer_offset, element_size, byte_t{0});
+                } else {
+                    io.SetPosition(*element_file_offset);
+                    io.ReadBuffer(cstd::span(buffer.data() + buffer_offset, element_size));
+                }
+                return {};
+            });
+        if (!process_result) {
+            return cstd::unexpected(process_result.error());
+        }
+    } else {
+        return hdf5::error(hdf5::HDF5ErrorCode::InvalidVariantState, "Unknown storage type in dataset");
+    }
+
+    return {};
 }
 
 __device__
@@ -36,5 +318,89 @@ hdf5::expected<void> Dataset::WriteHyperslab(
     const hdf5::dim_vector<uint64_t>& stride,
     const hdf5::dim_vector<uint64_t>& block
 ) const {
-    return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "Dataset::WriteHyperslab temporarily disabled");
+    if (type_.class_v == DatatypeMessage::Class::kVariableLength) {
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Variable length datatypes are not supported yet");
+    }
+
+    hdf5::dim_vector<uint64_t> dataset_dims(space_.dimensions.size());
+
+    cstd::transform(
+        space_.dimensions.begin(),
+        space_.dimensions.end(),
+        dataset_dims.begin(),
+        [](const auto& dim_info) { return dim_info.size; }
+    );
+
+    auto iterator_result = HyperslabIterator::New(start, count, stride, block, dataset_dims);
+    if (!iterator_result) {
+        return cstd::unexpected(iterator_result.error());
+    }
+    HyperslabIterator iterator = cstd::move(*iterator_result);
+
+    size_t element_size = type_.Size();
+    auto total_elements_result = iterator.GetTotalElements();
+
+    if (!total_elements_result) {
+        return cstd::unexpected(total_elements_result.error());
+    }
+
+    uint64_t total_elements = *total_elements_result;
+
+    if (data.size() < total_elements * element_size) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferTooSmall, "Data buffer too small for hyperslab");
+    }
+    if (data.size() > total_elements * element_size) {
+        return hdf5::error(hdf5::HDF5ErrorCode::BufferTooLarge, "Data buffer size exceeds hyperslab size");
+    }
+
+    auto props = layout_.properties;
+
+    if (const auto* compact = cstd::get_if<CompactStorageProperty>(&props)) {
+        // Compact storage is read-only after creation, so writing is not supported
+        return hdf5::error(hdf5::HDF5ErrorCode::FeatureNotSupported, "Cannot write to compact storage dataset");
+
+    } else if (const auto* contiguous = cstd::get_if<ContiguousStorageProperty>(&props)) {
+        size_t data_offset = 0;
+        auto io = object_.file->MakeRW();
+
+        while (!iterator.IsAtEnd()) {
+            auto linear_index = iterator.GetLinearIndex();
+
+            if (!linear_index) {
+                return cstd::unexpected(linear_index.error());
+            }
+
+            offset_t file_offset = contiguous->address + *linear_index * element_size;
+
+            io.SetPosition(file_offset);
+            io.WriteBuffer(cstd::span(data.data() + data_offset, element_size));
+
+            data_offset += element_size;
+            iterator.Advance();
+        }
+
+    } else if (const auto* chunked = cstd::get_if<ChunkedStorageProperty>(&props)) {
+        auto io = object_.file->MakeRW();
+        auto process_result = ProcessChunkedHyperslab(
+            chunked, iterator, element_size, object_.file,
+            [&](const cstd::optional<offset_t>& element_file_offset, size_t buffer_offset, const ChunkCoordinates& /* chunk_coords */) -> hdf5::expected<void> {
+                if (!element_file_offset.has_value()) {
+                    // Chunk doesn't exist (sparse dataset) - this is an error for writing
+                    // In HDF5, writing to a non-existent chunk should create the chunk,
+                    // but our current implementation doesn't support chunk creation
+                    return hdf5::error(hdf5::HDF5ErrorCode::NotImplemented, "Cannot write to non-existent chunk (chunk creation not implemented)");
+                }
+
+                io.SetPosition(*element_file_offset);
+                io.WriteBuffer(cstd::span(data.data() + buffer_offset, element_size));
+                return {};
+            });
+        if (!process_result) {
+            return cstd::unexpected(process_result.error());
+        }
+    } else {
+        return hdf5::error(hdf5::HDF5ErrorCode::InvalidVariantState, "Unknown storage type in dataset");
+    }
+
+    return {};
 }
