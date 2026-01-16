23d5079bbe36ff5199e2091ef8429bd8c7c92510
abscosmos <joshua@dinoslice.com>
2026-01-16 00:06:27 +0000

    Switch allocator to buddy allocator

--- src/hdf5/gpu_allocator.h
+++ src/hdf5/gpu_allocator.h
@@ -2,12 +2,12 @@
 
 #include "../cuda_compat.h"
 
-#include "hermes_shm/memory/allocator/arena_allocator.h"
+#include "hermes_shm/memory/allocator/buddy_allocator.h"
 #include "hermes_shm/data_structures/priv/vector.h"
 
 namespace hdf5 {
 
-    using HdfAllocator = hshm::ipc::ArenaAllocator<false>;
+    using HdfAllocator = hshm::ipc::BuddyAllocator;
 
     template<typename T>
     using vector = hshm::priv::vector<T, HdfAllocator>;
