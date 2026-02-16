6db2b5b8e81b067ec4d1c52e3c241ceccb76f62b
abscosmos <joshua@dinoslice.com>
2026-02-16 10:49:26 -0600

    Create simple concept-based serialization system
    Taken from 'hdf5-gpu' project with some tweaks

---
+++ libs/kvhdf5/src/serde.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "defines.h"
+#include <cuda/std/span>
+#include <cuda/std/type_traits>
+
+namespace serde {
+
+template<typename T>
+concept IsPOD = cstd::is_trivially_copyable_v<T> && cstd::is_standard_layout_v<T>;
+
+template<typename T>
+struct SerializePOD : cstd::false_type {};
+
+template<typename S>
+concept Serializer = requires(S&& s, cstd::span<const kvhdf5::byte_t> data) {
+    { s.WriteBuffer(data) } -> cstd::same_as<void>;
+};
+
+template<typename D>
+concept Deserializer = requires(D&& d, cstd::span<kvhdf5::byte_t> data) {
+    { d.ReadBuffer(data) } -> cstd::same_as<void>;
+};
+
+template<typename S>
+concept Seekable = requires(S&& s, size_t pos) {
+    { s.GetPosition() } -> cstd::same_as<size_t>;
+    { s.SetPosition(pos) } -> cstd::same_as<void>;
+};
+
+template<typename T, Serializer S> requires (SerializePOD<T>::value && IsPOD<T>)
+CROSS_FUN void Write(S&& s, const T& data) {
+    s.WriteBuffer(cstd::as_bytes(cstd::span(&data, 1)));
+}
+
+template<typename T, Deserializer D> requires (SerializePOD<T>::value && IsPOD<T>)
+CROSS_FUN T Read(D&& d) {
+    T out;
+    d.ReadBuffer(cstd::as_writable_bytes(cstd::span(&out, 1)));
+    return out;
+}
+
+template<Seekable S>
+CROSS_FUN void Skip(S&& s, size_t count) {
+    size_t start = s.GetPosition();
+    s.SetPosition(start + count);
+}
+
+template<typename T, Seekable S>
+CROSS_FUN void Skip(S&& s) {
+    Skip(s, sizeof(T));
+}
+
+} // namespace serde
+
+#define KVHDF5_AUTO_SERDE(T) \
+    static_assert(serde::IsPOD<T>, #T " must be POD to enable auto serde"); \
+    template<> struct serde::SerializePOD<T> : cstd::true_type {}
+
+// opt in for built-in types
+KVHDF5_AUTO_SERDE(uint8_t);
+KVHDF5_AUTO_SERDE(uint16_t);
+KVHDF5_AUTO_SERDE(uint32_t);
+KVHDF5_AUTO_SERDE(uint64_t);
+KVHDF5_AUTO_SERDE(int8_t);
+KVHDF5_AUTO_SERDE(int16_t);
+KVHDF5_AUTO_SERDE(int32_t);
+KVHDF5_AUTO_SERDE(int64_t);
+KVHDF5_AUTO_SERDE(float);
+KVHDF5_AUTO_SERDE(double);
+KVHDF5_AUTO_SERDE(bool);
+KVHDF5_AUTO_SERDE(char);
