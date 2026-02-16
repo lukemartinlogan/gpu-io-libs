ee9ba77e9103329a6eb72f13a8c0d011aee03347
abscosmos <joshua@dinoslice.com>
2026-02-16 10:48:29 -0600

    Create type for errors

---
+++ libs/kvhdf5/src/kvhdf5/error.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "defines.h"
+#include "utils/gpu_string.h"
+#include <cuda/std/expected>
+
+namespace kvhdf5 {
+
+enum class ErrorCode : uint8_t {
+    // Add error codes as needed during implementation
+};
+
+struct Error {
+    ErrorCode code;
+    gpu_string_view message;
+};
+
+template<typename T>
+using expected = cstd::expected<T, Error>;
+
+CROSS_FUN
+constexpr cstd::unexpected<Error> make_error(ErrorCode code, gpu_string_view msg = {}) {
+    return cstd::unexpected(Error{code, msg});
+}
+
+} // namespace kvhdf5
