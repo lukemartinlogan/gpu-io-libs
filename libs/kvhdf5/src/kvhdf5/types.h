006929c6e2675c68f832ef79c759f0df05831187
abscosmos <joshua@dinoslice.com>
2026-02-16 12:20:55 -0600

    Create header to include all types

---
+++ libs/kvhdf5/src/kvhdf5/types.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Convenience header that includes all type definitions
+
+#include "types/object_id.h"
+#include "types/group_id.h"
+#include "types/dataset_id.h"
+#include "types/datatype_id.h"
+#include "types/chunk_key.h"
