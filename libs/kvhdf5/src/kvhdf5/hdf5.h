f6ec90ee1f944c4235ccb7ef3b68cb9720d65638
abscosmos <joshua@dinoslice.com>
2026-02-28 16:05:09 -0600

    Add convenience header to use all hdf5 types

---
+++ libs/kvhdf5/src/kvhdf5/hdf5.h
@@ -0,0 +1,10 @@
+// hdf5.h — include-all for the public HDF5-like API
+#pragma once
+
+#include "ref.h"
+#include "dataspace.h"
+#include "hdf5_datatype.h"
+#include "file.h"
+#include "hdf5_group.h"
+#include "hdf5_dataset.h"
+#include "hdf5_attribute.h"
