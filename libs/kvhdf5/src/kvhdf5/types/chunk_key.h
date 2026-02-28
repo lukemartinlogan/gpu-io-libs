fbfaa0c88b9209b2e02b8bbcbad695459c652a2d
abscosmos <joshua@dinoslice.com>
2026-02-28 16:01:15 -0600

    Replace 'inplace_vector' in ChunkKey with array and size field
    'inplace_vector' had padding bytes that were causing issues with comparing by bytes

--- libs/kvhdf5/src/kvhdf5/types/chunk_key.h
+++ libs/kvhdf5/src/kvhdf5/types/chunk_key.h
@@ -3,36 +3,58 @@
 #include "dataset_id.h"
 #include "../../defines.h"
 #include "../../serde.h"
-#include <cuda/std/inplace_vector>
+#include <utils/algorithms.h>
+#include <cuda/std/array>
 #include <cuda/std/span>
 
 namespace kvhdf5 {
 
 struct ChunkKey {
-    DatasetId dataset;
-    cstd::inplace_vector<uint64_t, MAX_DIMS> coords;
+    DatasetId dataset{};
+    uint8_t ndims_{0};
+    padding<7> _pad;
+    cstd::array<uint64_t, MAX_DIMS> coords{};
 
-    CROSS_FUN constexpr ChunkKey() : dataset(), coords() {}
+    CROSS_FUN constexpr ChunkKey() = default;
 
     CROSS_FUN ChunkKey(DatasetId ds, cstd::span<const uint64_t> c)
-        : dataset(ds), coords(c.begin(), c.end()) {
+        : dataset(ds), ndims_(static_cast<uint8_t>(c.size()))
+    {
         KVHDF5_ASSERT(c.size() <= MAX_DIMS, "ChunkKey: span size exceeds MAX_DIMS");
+        algorithms::copy(c, cstd::span(coords.data(), c.size()));
     }
 
-    CROSS_FUN uint8_t ndims() const { return static_cast<uint8_t>(coords.size()); }
+    CROSS_FUN uint8_t ndims() const { return ndims_; }
+
+    CROSS_FUN cstd::span<const uint64_t> Coords() const {
+        return cstd::span(coords.data(), ndims_);
+    }
+
+    CROSS_FUN cstd::span<uint64_t> Coords() {
+        return cstd::span(coords.data(), ndims_);
+    }
 
     CROSS_FUN constexpr bool operator==(const ChunkKey& other) const {
-        return dataset == other.dataset && coords == other.coords;
+        if (dataset != other.dataset || ndims_ != other.ndims_) return false;
+
+        auto a = Coords();
+        auto b = other.Coords();
+
+        return cstd::equal(a.begin(), a.end(), b.begin());
     }
 
     CROSS_FUN constexpr bool operator!=(const ChunkKey& other) const {
         return !(*this == other);
     }
 
-    // Lexicographic ordering: first by dataset, then coords
     CROSS_FUN constexpr bool operator<(const ChunkKey& other) const {
         if (dataset != other.dataset) return dataset < other.dataset;
-        return coords < other.coords;
+        if (ndims_ != other.ndims_) return ndims_ < other.ndims_;
+        
+        auto a = Coords();
+        auto b = other.Coords();
+        
+        return cstd::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
     }
 
     CROSS_FUN constexpr bool operator<=(const ChunkKey& other) const {
@@ -50,5 +72,4 @@ struct ChunkKey {
 
 } // namespace kvhdf5
 
-// inplace_vector is POD-compatible, so we can use auto serde
 KVHDF5_AUTO_SERDE(kvhdf5::ChunkKey);
