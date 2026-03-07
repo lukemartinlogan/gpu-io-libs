6501581af93e7e8590314949ec2e92042863e2c0
abscosmos <joshua@dinoslice.com>
2026-03-07 13:44:02 -0600

    Use memcpy instead of loop to fix aliasing issues

--- libs/kvhdf5/src/utils/algorithms.h
+++ libs/kvhdf5/src/utils/algorithms.h
@@ -2,18 +2,17 @@
 
 #include "../defines.h"
 #include <cuda/std/span>
+#include <cuda/std/cstring>
 
 namespace algorithms {
 
-// Manual copy implementation (cuda::std::copy has issues in some contexts)
+// Copy between spans using memcpy (safe for type-punning, avoids TBAA issues)
 template<typename T>
 CROSS_FUN void copy(cstd::span<const T> src, cstd::span<T> dst) {
     // Assert that destination has enough space
     KVHDF5_ASSERT(dst.size() >= src.size(), "Destination span too small for copy");
 
-    for (size_t i = 0; i < src.size(); ++i) {
-        dst[i] = src[i];
-    }
+    cstd::memcpy(dst.data(), src.data(), src.size() * sizeof(T));
 }
 
 // Overload for non-const source (delegates to const version)
